b0c6bdd7ed2337e3ad9b2d81e35b2cdab32386ed
camilo avila <cajoavibustos@gmail.com>
2024-09-13 10:50:43 -0500

    Update PedIA.js

--- PedIA.js
+++ PedIA.js
@@ -1,3 +1,14 @@
+const container = document.querySelector('.container');
+const signupButton = document.querySelector('.signup-section header');
+const loginButton = document.querySelector('.login-section header');
+
+loginButton.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+signupButton.addEventListener('click', () => {
+    container.classList.remove('active');
+});
 // Verificar compatibilidad con SpeechRecognition
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
