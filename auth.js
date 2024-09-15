aff5c7200904bf2c38f1f42189c15710d5a2951a
camilo avila <cajoavibustos@gmail.com>
2024-09-15 10:17:15 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Configuración de Supabase
-const supabaseUrl = 'https://<project-ref>.supabase.co'
-const supabaseAnonKey = '<your-anon-key>'
+const supabaseUrl = 'https://itrtgoozuuygamciugrk.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0cnRnb296dXV5Z2FtY2l1Z3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYyNzYzNTIsImV4cCI6MjA0MTg1MjM1Mn0.sGWSOYHfflAXDmQUJp4ngx4Z0K4_YUhYU_hku77-B1Q'
 
 // Crea el cliente de Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
